7be6941e984764dafeed18aa1ee69380e0f05368
sonika <sonika.soni2@gmail.com>
2018-02-02 20:52:48 -0800

    Initial commit

---
+++ app/src/main/java/com/sonika/kotlinrxjavaexample/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.sonika.kotlinrxjavaexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tvHello: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvHello = findViewById<View>(R.id.tvHello) as TextView
+
+        showToastOnSubscribe()
+    }
+
+    fun showToastOnSubscribe(){
+        Observable.just("Welcome to RxJava")
+                .subscribeOn(Schedulers.newThread())
+                //each subscription is going to be on a new thread.
+                .observeOn(AndroidSchedulers.mainThread())
+        //observation on the main thread
+        //Now our subscriber!
+        .subscribe(object : Observer<String>{
+            override fun onComplete() {
+            }
+
+            override fun onNext(t: String) {
+
+                tvHello.setText(t)
+            }
+
+            override fun onError(e: Throwable) {
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+        })
+    }
+}
